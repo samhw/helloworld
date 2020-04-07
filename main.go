4c5aedd3a5b6390a917255beac46d37b0abbcee3
Sam Ng <samuelcng2@yahoo.com>
2020-04-07 01:03:01 -0700

    commit to trigger deployment update

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world 04/07!\n")
+		fmt.Fprintf(w, "Hello world 04/07.01!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
